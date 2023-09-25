bb7642f118d79859608f6c821db664b33f03d15d
Uchiha Itachi <danivetlyrics@gmail.com>
2023-09-25 14:46:21 +0100

    styles

--- src/app.js
+++ src/app.js
@@ -1,17 +1,73 @@
 const path = require('path');
+const hbs = require('hbs');
 const express = require('express');
-
-
-const publicDirectoryPath = path.join(__dirname, '../public');
 
 const app = express();
 
-app.use(express.static(publicDirectoryPath))
+//Define path for express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+
+//Setup handlebars engine  and views loaction
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+//register partials
+hbs.registerPartials(partialsPath);
+
+// setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index',{
+        pageTitle: 'Weather',
+        title: 'Weather',
+        name: 'Kamelot'
+    });
+});
+
+//About page
+app.get('/about', (req, res) => {
+    res.render('about',{
+        pageTitle: 'about',
+        title: 'About Page',
+        name: 'Kamelot'
+    });
+});
+
+//Help page
+app.get('/help', (req, res) => {
+    res.render('help',{
+        pageTitle: 'help',
+        title: 'Help Page',
+        helpText: 'This is some helpful text',
+        name: 'Kamelot'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
-        forecast: 'Snowy',
+        forecast: 'It is Snowing',
         location: 'Philadelphia'
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        pageTitle: '404',
+        title: '404 Page',
+        errorText: 'Help article not found',
+        name: 'Kamelot'
+    });
+});
+app.get('*', (req, res) => {
+    res.render('404', {
+        pageTitle: '404',
+        title: '404 Page',
+        errorText: 'Page not found',
+        name: 'Kamelot'
     });
 });
 
